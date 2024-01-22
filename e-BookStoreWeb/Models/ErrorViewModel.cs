f102afcd640264a3c67773861a09408df38950d0
Jovana Holjevac <holjevac.im4.2020@uns.ac.rs>
2024-01-22 16:36:53 +0100

    Add project files.

---
+++ e-BookStoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace e_BookStoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
