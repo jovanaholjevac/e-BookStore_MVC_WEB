11e8ac71ca5ba2f4052208a3621031455de7beee
Jovana Holjevac <holjevac.im4.2020@uns.ac.rs>
2024-01-23 01:23:38 +0100

    22.01.24

--- e-BookStoreWeb/Program.cs
+++ e-BookStoreWeb/Program.cs
@@ -1,7 +1,12 @@
+using e_BookStoreWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -24,4 +29,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+app.Run(); // runs the project
