d7b6d6771c718691c8818889773d0b49f0e24611
Jovana Holjevac <holjevac.im4.2020@uns.ac.rs>
2024-01-26 17:53:04 +0100

    finalna verzija

--- e-BookStoreWeb/Program.cs
+++ e-BookStoreWeb/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.Configure<StripeSettings>(builder.Configuration.GetSection("Stripe"));
+
 
 builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 builder.Services.ConfigureApplicationCookie(options => {
@@ -38,7 +38,6 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-StripeConfiguration.ApiKey = builder.Configuration.GetSection("Stripe:SecretKey").Get<string>();
 app.UseRouting();
 app.UseAuthentication(); //da li je user name i pass dobar
 app.UseAuthorization(); //u odnosu na autorizaciju nije isti pristup za sve korsinike
